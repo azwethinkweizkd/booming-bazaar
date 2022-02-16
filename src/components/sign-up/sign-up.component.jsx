8e887e5d84f2c6754f97f2220b0473182bb2509a
Kevin Devlin <azwethinkweizkd@gmail.com>
2022-02-15 22:19:19 -0700

    implemented firebase utils, included ability to store authenticated users into our firestore db

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+
+import "./sign-up.styles.scss";
+
+export default function SignUp() {
+  const [formState, setFormState] = useState({
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { displayName, email, password, confirmPassword } = formState;
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      console.log(user);
+
+      await createUserProfileDocument(user, { displayName });
+
+      setFormState({
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="sign-up">
+      <h2 className="title">I do not have an account</h2>
+      <span>Sign up with your email and password</span>
+      <form className="sign-up-form" onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          name="displayName"
+          value={formState.displayName}
+          onChange={handleChange}
+          placeholder="Diplay Name"
+          requied
+        />
+
+        <FormInput
+          type="email"
+          name="email"
+          value={formState.email}
+          onChange={handleChange}
+          placeholder="Email"
+          requied
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={formState.password}
+          onChange={handleChange}
+          placeholder="Password"
+          requied
+        />
+        <FormInput
+          type="password"
+          name="confirmPassword"
+          value={formState.confirmPassword}
+          onChange={handleChange}
+          placeholder="Confirm Password"
+          requied
+        />
+        <CustomButton type="submit">SIGN UP</CustomButton>
+      </form>
+    </div>
+  );
+}
