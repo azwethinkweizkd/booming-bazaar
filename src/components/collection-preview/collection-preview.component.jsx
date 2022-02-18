5f0086ec9e8e69d591195f62c47d511b1daf8df2
Kevin Devlin <azwethinkweizkd@gmail.com>
2022-02-17 20:32:46 -0700

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,15 +4,15 @@ import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
-  console.log(title, items);
+  // console.log(title, items);
   return (
     <div className="collection-preview">
       <h1 className="tittle">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map(({ id, ...otherItemProps }) => (
-            <CollectionItem key={id} {...otherItemProps} />
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
           ))}
       </div>
     </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import shopData from "../../utils/shop.data.json";
+import SHOP_DATA from "../../utils/shop.data.js";
 
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
@@ -8,7 +8,7 @@ export default function ShopPage() {
   // console.log(shopData[0]);
   return (
     <div className="shop-page">
-      {shopData.map(({ id, ...otherProps }) => (
+      {SHOP_DATA.map(({ id, ...otherProps }) => (
         <CollectionPreview key={id} {...otherProps} />
       ))}
     </div>
